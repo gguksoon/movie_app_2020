6e53b676877788795d1de5d3c37abf7eacdd14a7
msjo <msjo@letech.kr>
2022-04-25 15:29:03 +0900

    음식컴포넌트를 map()을 이용해 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+function Food({foodName, picture}) {
+  return (
+    <div>
+      <img src={picture} alt="HTML5" style={{width:200}}/>
+      <h3>I like {foodName}</h3>
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: 'Kimchi',
+    image: 'https://health.chosun.com/site/data/img_dir/2021/09/01/2021090100998_0.jpg'
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg'
+  },
+  {
+    name: 'Bibimbap',
+    image: 'https://cf.creatrip.com/original/blog/8937/166lv4gz4u0ei0nv8oykcq5t61w2zdtf.jpg'
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <h1>Hello, React!</h1>
+      {foodILike.map(dish => (
+        <Food key={dish.name} foodName={dish.name} picture={dish.image}/>
+      ))}
     </div>
   );
 }
